ed98e5054e55eff3ab1632fbe8f072eee1a9b6d1
the-chronicles <oluseungideon@gmail.com>
2023-12-20 12:35:16 +0100

    Added apis

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable */
+import { formatCurrency } from "../../utils/helpers";
+
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+import SearchOrder from "../features/order/SearchOrder"
+
+function Header() {
+    return <header>
+        <Link to='/'>Fast Pizza Co.</Link>
+
+        <SearchOrder />
+
+        <p>Seun</p>
+    </header>
+}
+
+export default Header
