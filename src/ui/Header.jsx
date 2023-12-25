44239725ef14fa63cf3ee66cfc80e32b789d8310
the-chronicles <oluseungideon@gmail.com>
2023-12-25 19:08:55 +0100

    Added Tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,19 @@
-import { Link } from "react-router-dom"
-import SearchOrder from "../features/order/SearchOrder"
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
-    return <header>
-        <Link to='/'>Fast Pizza Co.</Link>
+  return (
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast Pizza Co.
+      </Link>
 
-        <SearchOrder />
+      <SearchOrder />
 
-        <p>Seun</p>
+      <Username />
     </header>
+  );
 }
 
-export default Header
+export default Header;
