3208c51ad630cb38f864fbe9091011f6095c2e0f
the-chronicles <oluseungideon@gmail.com>
2024-04-03 17:42:11 +0100

    added menu style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      colors: {
+        pizza: "#123456",
+      },
+    },
   },
   plugins: [],
 };
