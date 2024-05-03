7fb05933dccac0bd187f2cc89d2ced75ef015711
the-chronicles <oluseungideon@gmail.com>
2024-05-03 23:53:40 +0100

    back

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,6 +2,7 @@
 import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
 
 const fakeCart = [
   {
@@ -32,16 +33,18 @@ function Cart() {
 
   return (
     <div>
-      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+      {/* <LinkButton to="/menu">&larr; Back to menu</LinkButton> */}
 
-      <h2>Your cart, %NAME%</h2>
+      {/* <h2>Your cart, %NAME%</h2> */}
 
       <div>
-        <Button to="/order/new" type="primary">
-          Order pizzas
-        </Button>
+        <div className="flex items-center">
+          <Button to="/order/new" type="primary">
+            Join Community <BsArrowUpRightCircleFill />
+          </Button>
+        </div>
 
-        <button>Clear cart</button>
+        {/* <button>Clear cart</button> */}
       </div>
     </div>
   );
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,15 +1,21 @@
 /* eslint-disable */
 
 import { Link } from "react-router-dom";
+import "./button.css";
+
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+    "inline-block rounded-full bg-orange-400 font-semibold tracking-wide text-white transition-colors duration-300 hover:bg-orange-400 focus:bg-orange-400 focus:outline-none focus:ring focus:ring-orange-400 focus:ring-offset-2 disabled:cursor-not-allowed focus:ring-animate gradient-ring";
+    // "inline-block rounded-full bg-orange-400 font-semibold tracking-wide text-white transition-colors duration-300 hover:bg-orange-400 focus:outline-none focus:ring focus:ring-orange-400 focus:ring-offset-2 disabled:cursor-not-allowed focus:ring-animate";
+    // "focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
-    primary: base + " px-4 py-3 md:px-6 md:py-4",
-    small: base + " py-2 md:px-5 md:py-2.5 px-4 text-xs",
+    primary: base + " px-4 py-3 md:px-6 md:py-4 gradient-ring",
+    small: base + " py-2 md:px-5 md:py-2.5 px-4 text-xs gradient-ring",
   };
+
+  
 
   if (to)
     return (
@@ -19,7 +25,7 @@ function Button({ children, disabled, to, type }) {
     );
   return (
     <button disabled={disabled} className={styles[type]}>
-      {children}
+      {children} 
     </button>
   );
 }
